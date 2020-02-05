de31ac795fb70f3a1e6d2d4de976b551553f7b38
edgar004 <ariel.rd.004@hotmail.com>
2020-02-05 05:50:35 +0100

    CRUD de categoria terminado

---
+++ GUI_V_2/FormCategorias.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace GUI_V_2
+{
+    partial class FormCategorias
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Size = new System.Drawing.Size(420, 31);
+            this.label1.Text = "CATEGORÍAS DE PRODUCTOS";
+            // 
+            // bnt_eliminar
+            // 
+            this.bnt_eliminar.FlatAppearance.BorderSize = 0;
+            this.bnt_eliminar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.bnt_eliminar.Location = new System.Drawing.Point(816, 444);
+            this.bnt_eliminar.Margin = new System.Windows.Forms.Padding(5);
+            this.bnt_eliminar.Click += new System.EventHandler(this.bnt_eliminar_Click);
+            // 
+            // btn_guardar
+            // 
+            this.btn_guardar.FlatAppearance.BorderSize = 0;
+            this.btn_guardar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.btn_guardar.Location = new System.Drawing.Point(816, 524);
+            this.btn_guardar.Margin = new System.Windows.Forms.Padding(5);
+            this.btn_guardar.Click += new System.EventHandler(this.btn_guardar_Click);
+            // 
+            // label19
+            // 
+            this.label19.Location = new System.Drawing.Point(59, 196);
+            this.label19.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label19.Click += new System.EventHandler(this.label19_Click);
+            // 
+            // label5
+            // 
+            this.label5.Text = "CÓDIGO";
+            // 
+            // estado
+            // 
+            this.estado.Location = new System.Drawing.Point(167, 190);
+            this.estado.Margin = new System.Windows.Forms.Padding(5);
+            this.estado.SelectedIndexChanged += new System.EventHandler(this.estado_SelectedIndexChanged);
+            // 
+            // Nom
+            // 
+            this.Nom.Limpiar = true;
+            this.Nom.Margin = new System.Windows.Forms.Padding(5);
+            this.Nom.Size = new System.Drawing.Size(277, 30);
+            // 
+            // Codigo
+            // 
+            this.Codigo.Limpiar = true;
+            this.Codigo.Margin = new System.Windows.Forms.Padding(5);
+            this.Codigo.Leave += new System.EventHandler(this.Codigo_Leave);
+            // 
+            // FormCategorias
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1144, 693);
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "FormCategorias";
+            this.Text = "Categorias";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+    }
+}
---
+++ GUI_V_2/FormCategorias.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI_V_2
+{
+    public partial class FormCategorias : FormBaseMan
+    {
+        int id = 0;
+        public FormCategorias()
+        {
+            InitializeComponent();
+        }
+
+        private void label19_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void estado_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_guardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               using (CRUD_MODEL tabla = new CRUD_MODEL())
+                {
+                    if (id ==0)
+                    {
+                        Categorias categoria = new Categorias
+                        {
+                            codigo = Codigo.Text.Trim(),
+                            nombre = Nom.Text.Trim(),
+                            estado = true
+                        };
+
+                        tabla.Categorias.Add(categoria);
+                    }
+
+                    else
+                    {
+                        var categoria = tabla.Categorias.FirstOrDefault(a => a.id == id);
+                        if (categoria != null)
+                        {
+                            categoria.estado = estado.SelectedIndex == 0 ? true : false;
+                            categoria.nombre = Nom.Text;
+                        }
+                    }
+
+                    tabla.SaveChanges();
+                    if (id == 0)
+                    {
+                        MessageBox.Show("La categoría se ha registrado correctamente.");
+                        id = 0;
+                    }
+                    else MessageBox.Show("La categoría se ha modificado correctamente.");
+                     
+                    Utilidades.LimpiarControles(this);
+                }
+                
+            }
+            catch (Exception ex)
+            {
+
+               //Guardar error
+            }
+        }
+
+        private void Codigo_Leave(object sender, EventArgs e)
+        {
+            if (Codigo.Text.Trim().Equals("") == false)
+            {
+                using (CRUD_MODEL tabla = new CRUD_MODEL())
+                {
+                    var categoria = tabla.Categorias.Where(a => a.codigo == Codigo.Text && a.estado == true).SingleOrDefault();
+                    if (categoria!=null)
+                    {
+                        Nom.Text = categoria.nombre;
+                        estado.SelectedIndex = 0;
+                        bnt_eliminar.Enabled = true;
+                        id = categoria.id;
+                   }
+                    
+                }
+            }
+        }
+
+        private void bnt_eliminar_Click(object sender, EventArgs e)
+        {
+            DialogResult res = MessageBox.Show("¿Seguro que quieres eliminar la categoría?", "Eliminar", MessageBoxButtons.YesNo);
+            if (res == DialogResult.No) return;
+            try
+            {
+                using (CRUD_MODEL tabla = new CRUD_MODEL())
+                {
+                    var categoria = tabla.Categorias.FirstOrDefault(a => a.id == id);
+                    if (categoria != null)
+                    {
+                        categoria.estado = false;
+                        tabla.SaveChanges();
+                        MessageBox.Show("La categoría se ha eliminado correctamente.");
+                        Utilidades.LimpiarControles(this);
+                        id = 0;
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+
+                //Guardar Error
+            }
+        }
+    }
+}
