cbd7a2890af2fdd4f77deba58e6824aa6fa742e9
edgar004 <ariel.rd.004@hotmail.com>
2020-02-16 23:36:34 +0100

    Consulta de clientes

--- GUI_V_2/ConsCli.Designer.cs
+++ GUI_V_2/ConsCli.Designer.cs
@@ -31,60 +31,66 @@
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.CodPro = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.NomPro = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridClientes = new System.Windows.Forms.DataGridView();
+            this.CodCli = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NomCli = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.CedCli = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.PrePro = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.CanPro = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.DesPro = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.TotalPro = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TelCli = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DirCli = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TipCli = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CorCli = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.estatus = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridClientes)).BeginInit();
             this.SuspendLayout();
             // 
             // label2
             // 
-            this.label2.Size = new System.Drawing.Size(114, 25);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Size = new System.Drawing.Size(148, 31);
             this.label2.Text = "CLIENTES";
             // 
             // button1
             // 
             this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button1.Location = new System.Drawing.Point(746, 113);
+            this.button1.Location = new System.Drawing.Point(995, 139);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             // 
-            // errorTxtBox1
+            // filtro
             // 
             this.filtro.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.filtro.Location = new System.Drawing.Point(142, 118);
+            this.filtro.Location = new System.Drawing.Point(189, 145);
+            this.filtro.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.filtro.KeyUp += new System.Windows.Forms.KeyEventHandler(this.filtro_KeyUp);
             // 
             // label16
             // 
             this.label16.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.label16.Location = new System.Drawing.Point(8, 123);
+            this.label16.Location = new System.Drawing.Point(11, 151);
+            this.label16.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             // 
             // btn_cerrar
             // 
-            this.btn_cerrar.Location = new System.Drawing.Point(916, 9);
+            this.btn_cerrar.Location = new System.Drawing.Point(1221, 11);
+            this.btn_cerrar.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             // 
             // btn_editar
             // 
-            this.btn_editar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btn_editar.Location = new System.Drawing.Point(605, 113);
+            this.btn_editar.Location = new System.Drawing.Point(807, 139);
+            this.btn_editar.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             // 
             // btn_nuevo
             // 
-            this.btn_nuevo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btn_nuevo.Location = new System.Drawing.Point(500, 113);
+            this.btn_nuevo.Location = new System.Drawing.Point(667, 139);
+            this.btn_nuevo.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.btn_nuevo.Click += new System.EventHandler(this.btn_nuevo_Click);
             // 
-            // dataGridView1
+            // dataGridClientes
             // 
-            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            this.dataGridClientes.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
-            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridClientes.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridClientes.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridClientes.BackgroundColor = System.Drawing.Color.White;
             dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopLeft;
             dataGridViewCellStyle7.BackColor = System.Drawing.Color.White;
             dataGridViewCellStyle7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -92,20 +98,21 @@
             dataGridViewCellStyle7.SelectionBackColor = System.Drawing.SystemColors.Highlight;
             dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.CodPro,
-            this.NomPro,
+            this.dataGridClientes.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
+            this.dataGridClientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridClientes.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.CodCli,
+            this.NomCli,
             this.CedCli,
-            this.PrePro,
-            this.CanPro,
-            this.DesPro,
-            this.TotalPro,
+            this.TelCli,
+            this.DirCli,
+            this.TipCli,
+            this.CorCli,
             this.estatus});
-            this.dataGridView1.Location = new System.Drawing.Point(11, 159);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Sunken;
+            this.dataGridClientes.Location = new System.Drawing.Point(12, 195);
+            this.dataGridClientes.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridClientes.Name = "dataGridClientes";
+            this.dataGridClientes.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Sunken;
             dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle8.BackColor = System.Drawing.SystemColors.Control;
             dataGridViewCellStyle8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -113,30 +120,30 @@
             dataGridViewCellStyle8.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
             dataGridViewCellStyle8.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle8;
-            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dataGridClientes.RowHeadersDefaultCellStyle = dataGridViewCellStyle8;
+            this.dataGridClientes.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
             dataGridViewCellStyle9.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle9.BackColor = System.Drawing.Color.White;
             dataGridViewCellStyle9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             dataGridViewCellStyle9.ForeColor = System.Drawing.SystemColors.MenuHighlight;
             dataGridViewCellStyle9.SelectionBackColor = System.Drawing.Color.Tomato;
-            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle9;
-            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dataGridView1.Size = new System.Drawing.Size(934, 374);
-            this.dataGridView1.TabIndex = 111;
+            this.dataGridClientes.RowsDefaultCellStyle = dataGridViewCellStyle9;
+            this.dataGridClientes.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridClientes.Size = new System.Drawing.Size(1245, 460);
+            this.dataGridClientes.TabIndex = 113;
             // 
-            // CodPro
+            // CodCli
             // 
-            this.CodPro.FillWeight = 51.84586F;
-            this.CodPro.HeaderText = "CODIGO";
-            this.CodPro.Name = "CodPro";
-            this.CodPro.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.CodCli.FillWeight = 51.84586F;
+            this.CodCli.HeaderText = "CÓDIGO";
+            this.CodCli.Name = "CodCli";
+            this.CodCli.Resizable = System.Windows.Forms.DataGridViewTriState.True;
             // 
-            // NomPro
+            // NomCli
             // 
-            this.NomPro.FillWeight = 162.959F;
-            this.NomPro.HeaderText = "NOMBRE";
-            this.NomPro.Name = "NomPro";
+            this.NomCli.FillWeight = 162.959F;
+            this.NomCli.HeaderText = "NOMBRE";
+            this.NomCli.Name = "NomCli";
             // 
             // CedCli
             // 
@@ -144,29 +151,29 @@
             this.CedCli.HeaderText = "CLA/RCN";
             this.CedCli.Name = "CedCli";
             // 
-            // PrePro
+            // TelCli
             // 
-            this.PrePro.FillWeight = 88.5353F;
-            this.PrePro.HeaderText = "TELEFONO";
-            this.PrePro.Name = "PrePro";
+            this.TelCli.FillWeight = 88.5353F;
+            this.TelCli.HeaderText = "TELÉFONO";
+            this.TelCli.Name = "TelCli";
             // 
-            // CanPro
+            // DirCli
             // 
-            this.CanPro.FillWeight = 184.5884F;
-            this.CanPro.HeaderText = "DIRECCION";
-            this.CanPro.Name = "CanPro";
+            this.DirCli.FillWeight = 184.5884F;
+            this.DirCli.HeaderText = "DIRECCIÓN";
+            this.DirCli.Name = "DirCli";
             // 
-            // DesPro
+            // TipCli
             // 
-            this.DesPro.FillWeight = 53.18349F;
-            this.DesPro.HeaderText = "TIPO";
-            this.DesPro.Name = "DesPro";
+            this.TipCli.FillWeight = 53.18349F;
+            this.TipCli.HeaderText = "TIPO";
+            this.TipCli.Name = "TipCli";
             // 
-            // TotalPro
+            // CorCli
             // 
-            this.TotalPro.FillWeight = 104.9513F;
-            this.TotalPro.HeaderText = "CORREO";
-            this.TotalPro.Name = "TotalPro";
+            this.CorCli.FillWeight = 104.9513F;
+            this.CorCli.HeaderText = "CORREO";
+            this.CorCli.Name = "CorCli";
             // 
             // estatus
             // 
@@ -176,12 +183,14 @@
             // 
             // ConsCli
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(958, 545);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(1277, 671);
+            this.Controls.Add(this.dataGridClientes);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "ConsCli";
             this.Text = "W";
+            this.Load += new System.EventHandler(this.ConsCli_Load);
             this.Controls.SetChildIndex(this.btn_editar, 0);
             this.Controls.SetChildIndex(this.btn_nuevo, 0);
             this.Controls.SetChildIndex(this.label2, 0);
@@ -189,8 +198,8 @@
             this.Controls.SetChildIndex(this.button1, 0);
             this.Controls.SetChildIndex(this.label16, 0);
             this.Controls.SetChildIndex(this.btn_cerrar, 0);
-            this.Controls.SetChildIndex(this.dataGridView1, 0);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Controls.SetChildIndex(this.dataGridClientes, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridClientes)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -198,14 +207,14 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn CodPro;
-        private System.Windows.Forms.DataGridViewTextBoxColumn NomPro;
+        private System.Windows.Forms.DataGridView dataGridClientes;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CodCli;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NomCli;
         private System.Windows.Forms.DataGridViewTextBoxColumn CedCli;
-        private System.Windows.Forms.DataGridViewTextBoxColumn PrePro;
-        private System.Windows.Forms.DataGridViewTextBoxColumn CanPro;
-        private System.Windows.Forms.DataGridViewTextBoxColumn DesPro;
-        private System.Windows.Forms.DataGridViewTextBoxColumn TotalPro;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TelCli;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DirCli;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TipCli;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CorCli;
         private System.Windows.Forms.DataGridViewTextBoxColumn estatus;
     }
 }
--- GUI_V_2/ConsCli.cs
+++ GUI_V_2/ConsCli.cs
@@ -15,7 +15,36 @@ namespace GUI_V_2.Consultas
         public ConsCli()
         {
             InitializeComponent();
+            dataGridClientes.AutoGenerateColumns = false;
+            CodCli.DataPropertyName = "codigo";
+            NomCli.DataPropertyName = "nombre_completo";
+            CedCli.DataPropertyName = "cla_rnc";
+            TelCli.DataPropertyName = "telefono";
+            DirCli.DataPropertyName = "direccion";
+            TipCli.DataPropertyName = "tipo_cliente";
+            CorCli.DataPropertyName = "correo";
+            estatus.DataPropertyName = "estado";
+            LlenarDataGrid();
+        }
 
+        public void LlenarDataGrid(string condicion = "")
+        {
+            using (CRUD_MODEL DB = new CRUD_MODEL())
+            {
+                IQueryable<Clientes> clientes = from c in DB.Clientes select c;
+                if (condicion.Equals(""))
+                {
+                    clientes = clientes.
+                    Where(c => c.estado == true);
+                }
+                else
+                {
+                    clientes = clientes.
+                    Where(c => (c.codigo.Contains(condicion) || c.nombre_completo.Contains(condicion) && c.estado == true));
+                }
+
+                dataGridClientes.DataSource = clientes.ToList();
+            }
 
         }
 
@@ -23,6 +52,16 @@ namespace GUI_V_2.Consultas
         {
             FormClientes obj = new FormClientes();
             obj.ShowDialog();
+        }
+
+        private void ConsCli_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void filtro_KeyUp(object sender, KeyEventArgs e)
+        {
+            LlenarDataGrid(filtro.Text.Trim());
         }
     }
 }
