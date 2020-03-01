6f2269e6f8901cb683c971661605e7b6af47f6d8
edgar004 <ariel.rd.004@hotmail.com>
2020-03-02 00:46:55 +0100

    Cambio en Clientes.cs relacionado con la ordenes en ventas

--- GUI_V_2/Clientes.cs
+++ GUI_V_2/Clientes.cs
@@ -18,6 +18,7 @@ namespace GUI_V_2
         public Clientes()
         {
             this.Facturas = new HashSet<Facturas>();
+            this.Ordenes_Reservadas = new HashSet<Ordenes_Reservadas>();
         }
     
         public int id { get; set; }
@@ -32,5 +33,7 @@ namespace GUI_V_2
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Facturas> Facturas { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Ordenes_Reservadas> Ordenes_Reservadas { get; set; }
     }
 }
