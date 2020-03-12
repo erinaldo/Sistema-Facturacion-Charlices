f0ea9e5695cb76c4af7ac635cf58e3cd5a43e044
sociio1908 <yeri.paulino@yahoo.es>
2020-03-12 16:08:04 -0400

    Ajuste muestra de filas en tabla Stock Bajos - InicioResumen

--- GUI_V_2/FormInicioResumen.Designer.cs
+++ GUI_V_2/FormInicioResumen.Designer.cs
@@ -333,7 +333,7 @@
             this.Cantidad,
             this.RedordenD,
             this.EstadoD});
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
             dataGridViewCellStyle2.BackColor = System.Drawing.Color.White;
             dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
