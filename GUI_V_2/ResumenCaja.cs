98e02b0a332d51ab06d498359a40d8d262c9c751
wartave <vatuta12@gmail.com>
2020-05-15 17:51:34 -0400

    Resumen de caja listo

--- GUI_V_2/ResumenCaja.cs
+++ GUI_V_2/ResumenCaja.cs
@@ -12,8 +12,10 @@ namespace GUI_V_2
 {
     public partial class ResumenCaja : Form
     {
-        public ResumenCaja()
+        public String tipo_resumen;
+        public ResumenCaja(String tipo_resumen="")
         {
+            this.tipo_resumen = tipo_resumen;
             InitializeComponent();
             ResumirCaja();
         }
@@ -28,23 +30,30 @@ namespace GUI_V_2
             this.Close();
         }
 
-        String Fecha;
-
 
         private void ResumirCaja()
         {
             try
             {
 
-                MessageBox.Show("ENTRO AL METODO.");
                 //entrada salida
                 using (CRUD_MODEL DB = new CRUD_MODEL())
                 {
-                    //c.fecha == Convert.ToDateTime(Fecha)
-                    MessageBox.Show("ENTRO A CALCULAR");
-                    IQueryable<Entrada_Salida_Caja> caja = DB.Entrada_Salida_Caja.Where(c => c.estado == true );
-                    MessageBox.Show("CANTIDAD DE CAJA: " + caja.ToList().Count);
-                    MessageBox.Show("Fecha en caja: " + caja.ToList()[0].fecha+" Fecha nuestra: "+Fecha);
+
+                    MessageBox.Show(tipo_resumen);
+                    if (tipo_resumen == "cierre_caja") {
+                        var res = DB.Procedimiento_total_caja(tipo_resumen, Utilidades.id_usuario);
+                        Total_Caja_Result totales_caja = res.FirstOrDefault();
+                        this.Cierre_Caja(totales_caja);
+                    }
+                    else
+                    {
+                        var res = DB.Resumen_caja();
+                        Resumen_caja_Result totales_caja = res.FirstOrDefault();
+                        this.ResumenVentas(totales_caja);
+                    }
+
+
 
                 }
 
@@ -56,37 +65,59 @@ namespace GUI_V_2
             {
                 //error
             }
-
-
-
-        }
-
-
-        //Valor monto apertura
-        private void MontoApertura()
-        {
-
-
-            
-        }
-
-        //Valor monto apertura
-        private void ResumenVentas()
-        {
-
-            using (CRUD_MODEL DB = new CRUD_MODEL())
+            finally
             {
-                //c.fecha == Convert.ToDateTime(Fecha)
-                MessageBox.Show("ENTRO A CALCULAR");
-                IQueryable<Entrada_Salida_Caja> caja = DB.Entrada_Salida_Caja.Where(c => c.estado == true);
-               
+                tipo_resumen = "";
             }
 
 
+
         }
 
 
+       
+        private void Cierre_Caja(Total_Caja_Result totales_caja)
+        {
+            total_efectivo.Text = totales_caja.total_ventas_efectivo.ToString();
+            anuladas.Text = totales_caja.anulada.ToString();
+            totales_ventas.Text = (totales_caja.total_ventas_efectivo + totales_caja.apertura_caja).ToString();
+
+            //movimiento caja
+            apertura.Text = totales_caja.apertura_caja.ToString();
+            entradas.Text = totales_caja.entrada_caja.ToString();
+            salidas.Text = totales_caja.salida_caja.ToString();
+            total_movimiento_caja.Text = ((totales_caja.apertura_caja + totales_caja.entrada_caja) - totales_caja.salida_caja).ToString();
+
+            total_efectivo_caja.Text = totales_caja.total_efectivo_caja.ToString();
+
+        }
+
+        private void ResumenVentas(Resumen_caja_Result totales_caja)
+        {
+
+            totales_ventas.Text = totales_caja.total_ventas.ToString();
+            label4.Visible = true;
+            disponibles.Visible = true;
+            disponibles.Text = totales_caja.total_disponible.ToString();
+            anuladas.Text = totales_caja.anulada.ToString();
 
 
+            total_efectivo.Text = totales_caja.total_ventas_efectivo.ToString();
+
+            //movimiento caja
+            apertura.Text = totales_caja.apertura_caja.ToString();
+            entradas.Text = totales_caja.entrada_caja.ToString();
+            salidas.Text = totales_caja.salida_caja.ToString();
+            total_movimiento_caja.Text = ((totales_caja.apertura_caja + totales_caja.entrada_caja) - totales_caja.salida_caja).ToString();
+
+            total_efectivo_caja.Text = (Double.Parse(total_movimiento_caja.Text) + Double.Parse(total_efectivo.Text)).ToString();
+
+
+        }
+
+        private void ResumenCaja_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
